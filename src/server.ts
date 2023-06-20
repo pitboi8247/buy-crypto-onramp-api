2c172aeb2a600fce63b9e00d8fba07281048890c
BingBong <bingbong@pancakseswap.com>
2023-06-20 03:17:14 +0100

    yarn lock fix

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import { Server } from 'http';
 import app from './app';
-// import logger from './utils/logger';
+import logger from './utils/logger';
 
 const port = Number(process.env.PORT);
 
 const server: Server = app.listen(port, (): void => {
-  console.log(`Aapplication listens on PORT: ${port}`);
+  logger.info(`Aapplication listens on PORT: ${port}`);
 });
 
 
 process.on('SIGTERM', () => {
-  console.log('SIGTERM received');
+  logger.info('SIGTERM received');
   if (server) {
     server.close();
   }
