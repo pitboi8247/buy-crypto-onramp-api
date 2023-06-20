a6ccaf958391323d6d2e23d374b70b4d6d220477
BingBong <bingbong@pancakseswap.com>
2023-06-20 03:01:03 +0100

    package size

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import { Server } from 'http';
 import app from './app';
-import logger from './utils/logger';
+// import logger from './utils/logger';
 
 const port = Number(process.env.PORT);
 
 const server: Server = app.listen(port, (): void => {
-  logger.info(`Aapplication listens on PORT: ${port}`);
+  console.log(`Aapplication listens on PORT: ${port}`);
 });
 
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM received');
+  console.log('SIGTERM received');
   if (server) {
     server.close();
   }
