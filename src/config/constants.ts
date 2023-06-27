b1f5044cfd9e184c94b43a64b7fbb8ea31cbf30c
BingBong <bingbong@pancakseswap.com>
2023-06-27 10:25:09 +0100

    fix url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import config from "./config";
 
-export const MOONPAY_URL = `https://buy-sandbox.moonpay.com?apiKey=${config.moonpayLiveKey}`;
+export const MOONPAY_URL = `https://buy.moonpay.com?apiKey=${config.moonpayLiveKey}`;
 export const BINANCE_CONNECT_URL = 'https://www.binancecnt.com/en/pre-connect';
 export const MERCURYO_URL = '/api-docs';
 export const BINANCE_CONNECT_API_URL = ""
